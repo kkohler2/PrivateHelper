6372035796e561cfc9f1de7b8ca1f6fac232cf22
Ken Kohler <kenkohler@hotmail.com>
2019-03-19 22:41:00 -0500

    Code Coverage changes Nuget updates

---
+++ codecov.ps1
@@ -0,0 +1,11 @@
+param
+(
+  $token
+)
+
+$ver = (gci "$env:userprofile\.nuget\packages\codecov").Name
+$cmd = "$env:userprofile\.nuget\packages\codecov\$ver\tools\codecov.exe";
+$fName = ".\UnitTests\coverage.opencover.xml";
+$arg1 = "-f ""$fName""";
+$arg2 = "-t $token";
+& $cmd $arg1 $arg2
